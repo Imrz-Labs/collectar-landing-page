f2dd2e8f44ca30aebde1d72820adae24e19d989b
Ben Emma <emmaben676@gmail.com>
2022-10-31 19:55:26 +0000

    next config

--- next.config.js
+++ next.config.js
@@ -1,31 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
-module.exports = {
- 
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+   eslint: { ignoreDuringBuilds: true },
+   typescript: { ignoreBuildErrors: true },
+};
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+module.exports = nextConfig;
 
-  typescript: {
-    ignoreBuildErrors: true,
-  },
- 
-}
 
